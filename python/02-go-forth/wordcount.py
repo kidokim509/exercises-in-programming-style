358bd336dce332a650b46649ac5ff7f24bd591b0
olaf.kido <olaf.kido@kakaocorp.com>
2018-06-26 23:11:27 +0900

    chapter02 2nd commit

--- python/02-go-forth/wordcount.py
+++ python/02-go-forth/wordcount.py
@@ -66,3 +66,46 @@ def remove_stop_words():
 			heap['words'].append(stack.pop()) # 꺼낸 후 저장한다.
 	stack.extend(heap['words']) # 단어를 스택에 적재한다.
 	del heap['stop_words']; del heap['words'] # 불필요하다.
+
+del frequencies():
+	"""
+	Takes a list of words and returns a dictionary associating
+	words with frequencies of occurrence
+	"""
+	heap['word_freqs'] = {}
+	# 실제 '포스(Forth)' 형식의 특색이 약간 나타나는 부분은 여기서부터 ...
+	while len(stack) > 0:
+		#... 이지만 다음 줄은 형식에 속하지 않는다.
+		# 기교 없이 순진한 구현 내용은 너무 느리다
+		if stack[-1] in heap['word_freqs']:
+			# 빈도를 증가시키며, 다음과 같은 후위 형식이가: f 1 +
+			stack.append(head['word_freqs'][stack[-1]]) # f를 넣는다
+			stack.append(1) # 1을 넣는다
+			stack.append(stack.pop() + stack.pop()) # 더한다
+		else:
+			stack.append(1) # stack[2]에 1을 넣는다
+		# 갱신한 빈도를 힙에 다시 적재한다
+		heap['word_freqs'][stack.pop()] = stack.pop()
+
+	# 그 결과를 스텍에 넣는다
+	stack.append(heap['word_freqs'])
+	del heap['word_freqs'] # 이 변수는 더 이상 필요치 않다
+
+def sort():
+	# 형식에 속하지 않는다. 연습 문제로 남겨 둔다
+	stack.extend(sortes(stack.pop().iteritems(), key=operator.itemgetter(1)))
+
+# 주 함수
+#
+stack.append(sys.argv[1])
+read_file(); filter_chars(); scan(); remove_stop_words()
+frequencies(); sort()
+
+stack.append(0)
+# 스택의 길이를 1을 기준으로 검사한다. 처리를 마친 후
+# 남아 있을 항목 하나는 마지막 단어가 될 것이기 때문이다
+while stack[-1] < 25 and len(stack) > 1:
+	heap['i'] = stack.pop()
+	(w, f) = stack.pop(); print w, ' - ', f
+	stack.append(heap['i']); stack.append(1)
+	stack.append(stack.pop() + stack.pop())
